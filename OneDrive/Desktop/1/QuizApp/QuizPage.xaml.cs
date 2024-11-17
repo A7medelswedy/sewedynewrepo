3c38f7e5be891fae267df2dada1d97775de2a7d5
AHmed-ELsewedy <ahmedkhaledelswedy@gmail.com>
2024-11-17 20:08:30 +0200

    firstcommit

---
+++ OneDrive/Desktop/1/QuizApp/QuizPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace QuizApp
+{
+    /// <summary>
+    /// Interaction logic for QuizPage.xaml
+    /// </summary>
+    public partial class QuizPage : Window
+    {
+        private List<string> questions; 
+        private List<List<string>> answerOptions; 
+        private List<int> correctAnswers; 
+        private int Questionnumber;
+        private int score;
+
+        public QuizPage()
+        {
+            InitializeComponent();
+            LoadQuestions();
+            DisplayQuestion();
+        }
+
+        private void LoadQuestions()
+        {
+            questions = new List<string>
+            {
+                "What does CPU stand for?",
+                "Which language is used for web apps?",
+                "What is the capital of France?"
+            };
+
+            answerOptions = new List<List<string>>
+            {
+                new List<string> { "Central Processing Unit", "Computer Personal Unit", "Central Print Unit" },
+                new List<string> { "Python", "JavaScript", "C#" },
+                new List<string> { "Berlin", "Madrid", "Paris" }
+            };
+
+            correctAnswers = new List<int> { 0, 1, 2 }; 
+
+            Questionnumber = 0;
+            score = 0;
+        }
+
+        private void DisplayQuestion()
+        {
+            var question = questions[Questionnumber];
+            QuestionDisplay.Text = question;
+            AnswerOptions.Children.Clear();
+
+            // Add new answers
+            foreach (var option in answerOptions[Questionnumber])
+            {
+                RadioButton radioButton = new RadioButton
+                {
+                    Content = option,
+                    Tag = answerOptions[Questionnumber].IndexOf(option) // Use IndexOf to get the index
+                };
+                AnswerOptions.Children.Add(radioButton);
+            }
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (RadioButton radioButton in AnswerOptions.Children)
+            {
+                if (radioButton.IsChecked == true && (int)radioButton.Tag == correctAnswers[Questionnumber])
+                {
+                    score++;
+                }
+            }
+
+            Questionnumber++;
+            if (Questionnumber < questions.Count)
+            {
+                DisplayQuestion();
+            }
+            else
+            {
+                ShowResults();
+            }
+        }
+
+        private void ShowResults()
+        {
+            ResultsPage resultsPage = new ResultsPage(score, questions.Count);
+            resultsPage.Show();
+            this.Close();
+        }
+    }
+}
---
+++ OneDrive/Desktop/1/QuizApp/ResultsPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuizApp
+{
+    /// <summary>
+    /// Interaction logic for ResultsPage.xaml
+    /// </summary>
+    public partial class ResultsPage : Window
+    {
+        public ResultsPage(int score, int totalQuestions)
+        {
+            InitializeComponent();
+            ScoreText.Text = $"Your Score: {score} out of {totalQuestions}";
+        }
+
+        private void RestartButton_Click(object sender, RoutedEventArgs e)
+        {
+            WelcomePage welcomePage = new WelcomePage();
+            welcomePage.Show();
+            this.Close();
+        }
+    }
+}
